3b487eaacd66340e4a481e269f63dabcadfe6b16
Dan Bennett <dghbennett47@gmail.com>
2025-03-28 12:21:01 +0000

    chore: monthly rent function

---
+++ src/getMonthlyRentPerTenant.ts
@@ -0,0 +1,24 @@
+import { parseProperties, parseTenants } from './utils';
+
+export async function getMonthlyRentPerTenant(propertyId: string) {
+    const properties = await parseProperties();
+    const tenants = await parseTenants();
+
+    const property = properties.find((p) => p.id === propertyId);
+
+    if (!property) {
+        throw Error('Property not found');
+    }
+
+    const totalRent = property.monthlyRentPence;
+
+    const numberOfTenants = tenants.filter(
+        (t) => t.propertyId === propertyId
+    ).length;
+
+    if (numberOfTenants === 0) {
+        throw Error('No tenants in property');
+    }
+
+    return Math.ceil(totalRent / numberOfTenants);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { getAverageRent } from './getAverageRent';
+import { getMonthlyRentPerTenant } from './getMonthlyRentPerTenant';
 
 export function runGetAverageRent() {
     getAverageRent('ENGLAND').then((averageRent) => {
         console.log('averageRent: ', averageRent);
+    });
+}
+
+// no tenants at p_1029
+export function runGetMonthlyRentPerTenant() {
+    getMonthlyRentPerTenant('p_1029').then((rent) => {
+        console.log('rent: ', rent);
     });
 }
